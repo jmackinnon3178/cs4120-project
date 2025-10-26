9306115f01c577a8874f260f80ad93490ec25f7d
josh <joshmackinnon8@gmail.com>
2025-10-26 16:50:57 -0300

    add validation set to split

--- src/data.py
+++ src/data.py
@@ -8,7 +8,9 @@ class Data:
         self.X: df = self.data[self.data.columns[:-1]]
         self.y: df = self.data[['G3']]
 
-    def split_data(self, test_size: float, random_state: int) -> tuple[df, df, df, df]:
-        X_train, X_test, y_train, y_test = \
-            model_selection.train_test_split(self.X, self.y, test_size=test_size, random_state=random_state)
-        return X_train, X_test, y_train, y_test
+    def split_data(self, train_ratio: float, test_ratio: float, val_ratio: float, random_state: int):
+        X_train, X_test, y_train, y_test = model_selection.train_test_split(self.X, self.y, test_size=1 - train_ratio, random_state=random_state)
+        X_val, X_test, y_val, y_test = model_selection.train_test_split(X_test, y_test, test_size=test_ratio/(test_ratio + val_ratio), random_state=random_state) 
+        
+        return X_train, X_test, X_val, y_train, y_test, y_val
+
