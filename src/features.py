28d9daf6eea4612d511feb6be0e87aaa7f22da82
josh <joshmackinnon8@gmail.com>
2025-10-28 19:44:13 -0300

    reorganize

--- src/features.py
+++ src/features.py
@@ -2,22 +2,18 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder
 from sklearn.compose import ColumnTransformer
 from pandas import DataFrame as df
 
-class Features:
-    def __init__(self) -> None:
     
-        self.__feature_cols_numeric = ['age','failures','absences','G1','G2']
+feature_cols_numeric = ['age','failures','absences','G1','G2']
+feature_cols_categorical = ['school','sex','address','famsize','Pstatus','Mjob','Fjob','reason','guardian','schoolsup','famsup','paid','activities','nursery','higher','internet','romantic']
+feature_cols_ordinal = ['Medu','Fedu','traveltime','studytime','famrel','freetime','goout','Dalc','Walc','health']
 
-        self.__feature_cols_categorical = ['school','sex','address','famsize','Pstatus','Mjob','Fjob','reason','guardian','schoolsup','famsup','paid','activities','nursery','higher','internet','romantic']
+lr_preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', StandardScaler(), feature_cols_numeric),
+        ('cat', OneHotEncoder(drop='if_binary'), feature_cols_categorical),
+        ('ord', OrdinalEncoder(), feature_cols_ordinal)
+    ],
+)
 
-        self.__feature_cols_ordinal = ['Medu','Fedu','traveltime','studytime','famrel','freetime','goout','Dalc','Walc','health']
-
-        self.preprocessor = ColumnTransformer(
-            transformers=[
-                ('num', StandardScaler(), self.__feature_cols_numeric),
-                ('cat', OneHotEncoder(drop='if_binary'), self.__feature_cols_categorical),
-                ('ord', OrdinalEncoder(), self.__feature_cols_ordinal)
-            ],
-        )
-
-    def grade_to_pass_fail(self, target: df) -> df:
-        return (target >= 10).astype(int)
+def grade_to_pass_fail(target: df) -> df:
+    return (target >= 10).astype(int)
