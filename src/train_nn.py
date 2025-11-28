2f9b57254a9ba0c0b1aed367871cba1d99b7777a
josh <joshmackinnon8@gmail.com>
2025-11-27 21:19:30 -0400

    final nns working

--- src/train_nn.py
+++ src/train_nn.py
@@ -14,29 +14,7 @@ random_state = 1
 clf_data = data.Data()
 reg_data = data.Data()
 
-def nn_clf_baseline():
-    scoring = {"accuracy": "accuracy_score", "f1": "f1_score"}
-    clf_data = data.Data()
-    X_train, X_test, y_train, y_test = clf_data.train_test_split(test_ratio=0.4, random_state=random_state, clf=True)
-    cv = make_cv(y_train, random_state=random_state)
-
-    pipelines = {
-            "clf_pipeline_32_16": Pipeline([
-                ("preprocessor", lr_prep_stdscaler),
-                ("clf", MLPClassifier(hidden_layer_sizes=(32, 16),random_state=random_state, max_iter=1000))
-            ]),
-            "clf_pipeline_64_32": Pipeline([
-                ("preprocessor", lr_prep_stdscaler),
-                ("clf", MLPClassifier(hidden_layer_sizes=(64, 32),random_state=random_state, max_iter=1000))
-            ])
-    }
-
-    cv_rows = cross_val(X_train, y_train, pipelines, scoring, cv)
-    print(parse_results(cv_rows, False))
-
-def nn_clf():
-    clf_data = data.Data()
-    # X_train, X_test, y_train, y_test = clf_data.train_test_split(test_ratio=0.4, random_state=random_state, clf=True)
+def nn_clf_final():
     X_train, X_test, X_val, y_train, y_test, y_val = clf_data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=True)
 
     X_train = lr_prep_stdscaler.fit_transform(X_train)
@@ -44,25 +22,29 @@ def nn_clf():
 
     model = models.Sequential([
         layers.Input(shape=(X_train.shape[1],)),
-        layers.Dense(32, activation='relu'),
-        layers.Dropout(0.3),
-        layers.Dense(16, activation='relu'),
+        layers.Dense(48, activation='relu'),
+        layers.Dropout(0.25),
+        layers.Dense(32, activation='sigmoid'),
         layers.Dropout(0.3),
         layers.Dense(1, activation='sigmoid')
     ])
 
-    print(model.summary())
-
     model.compile(
-        optimizer=optimizers.Adam(learning_rate=0.001),
+        optimizer=optimizers.Adam(learning_rate=0.010485843639257204),
         loss='binary_crossentropy',
         metrics=['accuracy', 'f1_score']
+    )
+
+    early_stop = callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=3
     )
 
     history = model.fit(
         X_train,
         y_train,
-        epochs=10,
+        epochs=32,
+        callbacks=[early_stop],
         validation_data=(X_val, y_val)
     )
 
@@ -78,7 +60,7 @@ def clf_optuna():
         model.add(
             layers.Dense(
                 units=trial.suggest_int("l1_units", 32, 64, step=8),
-                activation=trial.suggest_categorical("l1_activation", ["relu", "linear", "tanh", "sigmoid"])
+                activation=trial.suggest_categorical("l1_activation", ["relu", "sigmoid"])
             )
         )
         model.add(layers.Dropout(
@@ -88,7 +70,7 @@ def clf_optuna():
         model.add(
             layers.Dense(
                 units=trial.suggest_int("l2_units", 16, 32, step=8),
-                activation=trial.suggest_categorical("l2_activation", ["relu", "linear", "tanh", "sigmoid"])
+                activation=trial.suggest_categorical("l2_activation", ["relu", "sigmoid"])
             )
         )
         model.add(layers.Dropout(
@@ -143,29 +125,7 @@ def clf_optuna():
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
 
-def nn_reg_baseline():
-    scoring = {"mae": "neg_mean_absolute_error", "rmse": "neg_root_mean_squared_error"}
-    reg_data = data.Data()
-    X_train, X_test, y_train, y_test = reg_data.train_test_split(test_ratio=0.4, random_state=random_state, clf=False)
-    cv = make_cv(y_train, random_state=random_state)
-
-    pipelines = {
-            "reg_pipeline_32_16": Pipeline([
-                ("preprocessor", lr_prep_stdscaler),
-                ("regressor", MLPRegressor(hidden_layer_sizes=(32, 16),random_state=random_state, max_iter=10000))
-            ]),
-            "reg_pipeline_64_32": Pipeline([
-                ("preprocessor", lr_prep_stdscaler),
-                ("regressor", MLPRegressor(hidden_layer_sizes=(64, 32),random_state=random_state, max_iter=10000))
-            ])
-    }
-
-    cv_rows = cross_val(X_train, y_train, pipelines, scoring, cv)
-    print(parse_results(cv_rows, False))
-
-    
-def nn_reg():
-    # X_train, X_test, y_train, y_test = reg_data.train_test_split(test_ratio=0.4, random_state=random_state, clf=False)
+def nn_reg_final():
     X_train, X_test, X_val, y_train, y_test, y_val = reg_data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=False)
 
     X_train = lr_prep_stdscaler.fit_transform(X_train)
@@ -173,25 +133,29 @@ def nn_reg():
 
     model = models.Sequential([
         layers.Input(shape=(X_train.shape[1],)),
-        layers.Dense(32, activation='relu'),
-        layers.Dropout(0.3),
-        layers.Dense(16, activation='relu'),
-        layers.Dropout(0.3),
+        layers.Dense(40, activation='sigmoid'),
+        layers.Dropout(0.25),
+        layers.Dense(24, activation='relu'),
+        layers.Dropout(0.2),
         layers.Dense(1)
     ])
 
-    print(model.summary())
+    early_stop = callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=3
+    )
 
     model.compile(
-        optimizer=optimizers.Adam(learning_rate=0.001),
-        loss='mse',
-        metrics=['mae', metrics.RootMeanSquaredError()]
+        optimizer=optimizers.Adam(learning_rate=0.012745953868477105),
+        loss=losses.MeanSquaredError(),
+        metrics=[metrics.MeanAbsoluteError(), metrics.RootMeanSquaredError()]
     )
 
     history = model.fit(
         X_train,
         y_train,
-        epochs=100,
+        epochs=32,
+        callbacks=[early_stop],
         validation_data=(X_val, y_val)
     )
 
@@ -207,7 +171,7 @@ def reg_optuna():
         model.add(
             layers.Dense(
                 units=trial.suggest_int("l1_units", 32, 64, step=8),
-                activation=trial.suggest_categorical("l1_activation", ["relu", "linear", "tanh", "sigmoid"])
+                activation=trial.suggest_categorical("l1_activation", ["relu", "sigmoid"])
             )
         )
         model.add(layers.Dropout(
@@ -217,7 +181,7 @@ def reg_optuna():
         model.add(
             layers.Dense(
                 units=trial.suggest_int("l2_units", 16, 32, step=8),
-                activation=trial.suggest_categorical("l2_activation", ["relu", "linear", "tanh", "sigmoid"])
+                activation=trial.suggest_categorical("l2_activation", ["relu", "sigmoid"])
             )
         )
         model.add(layers.Dropout(
@@ -275,35 +239,8 @@ def reg_optuna():
 
 
 if __name__ == '__main__':
-    clf_optuna()
+    nn_clf_final()
+    nn_reg_final()
+    # clf_optuna()
     # reg_optuna()
-    # nn_clf()
-    # nn_clf_baseline()
-    # nn_reg()
-    # nn_reg_baseline()
 
-#clf best
-  #   Best trial:
-  # Value: 0.9538461565971375
-  # Params:
-  #   l1_units: 56
-  #   l1_activation: sigmoid
-  #   l1_drop_rate: 0.25
-  #   l2_units: 24
-  #   l2_activation: sigmoid
-  #   l2_drop_rate: 0.25
-  #   optimizer: Adam
-  #   learning_rate: 0.014227960702996326
-
-# best reg
-# Best trial:
-#   Value: 0.9086486101150513
-#   Params:
-#     l1_units: 48
-#     l1_activation: sigmoid
-#     l1_drop_rate: 0.25
-#     l2_units: 32
-#     l2_activation: linear
-#     l2_drop_rate: 0.35
-#     optimizer: Adam
-#     learning_rate: 0.007780603487032244
