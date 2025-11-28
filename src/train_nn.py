d323d4665f353c74b2f9cd2e90a9a4334f28ca67
josh <joshmackinnon8@gmail.com>
2025-11-27 21:53:27 -0400

    finall nns cleanup

--- src/train_nn.py
+++ src/train_nn.py
@@ -1,43 +1,37 @@
-from sklearn.neural_network import MLPClassifier, MLPRegressor
 import data
 from features import lr_prep_stdscaler
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import f1_score, accuracy_score
-from utils import cross_val, make_cv, parse_results
-import tensorflow as tf
 from keras import layers, metrics, models, optimizers, callbacks, utils, losses
 import optuna
 
 utils.set_random_seed(37)
 random_state = 1
 
-clf_data = data.Data()
-reg_data = data.Data()
+data = data.Data()
+X_train_clf, _, X_val_clf, y_train_clf, _, y_val_clf = data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=True)
+X_train_reg, _, X_val_reg, y_train_reg, _, y_val_reg = data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=False)
 
-def nn_clf_final():
-    X_train, X_test, X_val, y_train, y_test, y_val = clf_data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=True)
-
+def nn_clf_final(X_train, X_val, y_train, y_val):
     X_train = lr_prep_stdscaler.fit_transform(X_train)
     X_val = lr_prep_stdscaler.transform(X_val)
 
     model = models.Sequential([
         layers.Input(shape=(X_train.shape[1],)),
-        layers.Dense(48, activation='relu'),
+        layers.Dense(56, activation='relu'),
         layers.Dropout(0.25),
-        layers.Dense(32, activation='sigmoid'),
+        layers.Dense(16, activation='sigmoid'),
         layers.Dropout(0.3),
         layers.Dense(1, activation='sigmoid')
     ])
 
     model.compile(
-        optimizer=optimizers.Adam(learning_rate=0.010485843639257204),
+        optimizer=optimizers.Adam(learning_rate=0.004830884416995388),
         loss='binary_crossentropy',
         metrics=['accuracy', 'f1_score']
     )
 
     early_stop = callbacks.EarlyStopping(
         monitor='val_loss',
-        patience=3
+        patience=10
     )
 
     history = model.fit(
@@ -48,13 +42,48 @@ def nn_clf_final():
         validation_data=(X_val, y_val)
     )
 
-def clf_optuna():
+    return history
+
+def nn_reg_final(X_train, X_val, y_train, y_val):
+    X_train = lr_prep_stdscaler.fit_transform(X_train)
+    X_val = lr_prep_stdscaler.transform(X_val)
+
+    model = models.Sequential([
+        layers.Input(shape=(X_train.shape[1],)),
+        layers.Dense(56, activation='relu'),
+        layers.Dropout(0.3),
+        layers.Dense(24, activation='relu'),
+        layers.Dropout(0.25),
+        layers.Dense(1)
+    ])
+
+    early_stop = callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=10
+    )
+
+    model.compile(
+        optimizer=optimizers.Adam(learning_rate=0.029113754991306993),
+        loss=losses.MeanSquaredError(),
+        metrics=[metrics.MeanAbsoluteError(), metrics.RootMeanSquaredError()]
+    )
+
+    history = model.fit(
+        X_train,
+        y_train,
+        epochs=32,
+        callbacks=[early_stop],
+        validation_data=(X_val, y_val)
+
+    )
+
+    return history
+
+def clf_optuna(X_train, X_val, y_train, y_val):
+    X_train = lr_prep_stdscaler.fit_transform(X_train)
+    X_val = lr_prep_stdscaler.transform(X_val)
+
     def objective(trial):
-        X_train, X_test, X_val, y_train, y_test, y_val = clf_data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=True)
-
-        X_train = lr_prep_stdscaler.fit_transform(X_train)
-        X_val = lr_prep_stdscaler.transform(X_val)
-
         model = models.Sequential()
         model.add(layers.Input(shape=(X_train.shape[1],)))
         model.add(
@@ -83,7 +112,7 @@ def clf_optuna():
         learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-1, log=True)
         early_stop = callbacks.EarlyStopping(
             monitor='val_loss',
-            patience=3
+            patience=10
         )
 
         match opt:
@@ -125,47 +154,12 @@ def clf_optuna():
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
 
-def nn_reg_final():
-    X_train, X_test, X_val, y_train, y_test, y_val = reg_data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=False)
 
+def reg_optuna(X_train, X_val, y_train, y_val):
     X_train = lr_prep_stdscaler.fit_transform(X_train)
     X_val = lr_prep_stdscaler.transform(X_val)
 
-    model = models.Sequential([
-        layers.Input(shape=(X_train.shape[1],)),
-        layers.Dense(40, activation='sigmoid'),
-        layers.Dropout(0.25),
-        layers.Dense(24, activation='relu'),
-        layers.Dropout(0.2),
-        layers.Dense(1)
-    ])
-
-    early_stop = callbacks.EarlyStopping(
-        monitor='val_loss',
-        patience=3
-    )
-
-    model.compile(
-        optimizer=optimizers.Adam(learning_rate=0.012745953868477105),
-        loss=losses.MeanSquaredError(),
-        metrics=[metrics.MeanAbsoluteError(), metrics.RootMeanSquaredError()]
-    )
-
-    history = model.fit(
-        X_train,
-        y_train,
-        epochs=32,
-        callbacks=[early_stop],
-        validation_data=(X_val, y_val)
-    )
-
-def reg_optuna():
     def objective(trial):
-        X_train, X_test, X_val, y_train, y_test, y_val = reg_data.train_test_val_split(train_ratio=0.6, test_ratio=0.2, val_ratio=0.2, random_state=random_state, clf=False)
-
-        X_train = lr_prep_stdscaler.fit_transform(X_train)
-        X_val = lr_prep_stdscaler.transform(X_val)
-
         model = models.Sequential()
         model.add(layers.Input(shape=(X_train.shape[1],)))
         model.add(
@@ -194,7 +188,7 @@ def reg_optuna():
         learning_rate = trial.suggest_float("learning_rate", 1e-5, 1e-1, log=True)
         early_stop = callbacks.EarlyStopping(
             monitor='val_loss',
-            patience=3
+            patience=10
         )
 
         match opt:
@@ -237,10 +231,9 @@ def reg_optuna():
     for key, value in trial.params.items():
         print("    {}: {}".format(key, value))
 
-
 if __name__ == '__main__':
-    nn_clf_final()
-    nn_reg_final()
-    # clf_optuna()
-    # reg_optuna()
+    nn_clf_final(X_train_clf, X_val_clf, y_train_clf, y_val_clf)
+    nn_reg_final(X_train_reg, X_val_reg, y_train_reg, y_val_reg)
+    # clf_optuna(X_train_clf, X_val_clf, y_train_clf, y_val_clf)
+    # reg_optuna(X_train_reg, X_val_reg, y_train_reg, y_val_reg)
 
